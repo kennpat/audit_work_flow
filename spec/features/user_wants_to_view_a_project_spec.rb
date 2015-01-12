639703ab974108f436bbfbdb93c7f821e4cd06cb
kennpat <kennedypr@gmail.com>
2015-01-12 16:52:04 -0500

    added some navigational links to the site, as well as the show page for a project

---
+++ spec/features/user_wants_to_view_a_project_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+# It is likely this test will be heavily modified in the future
+
+feature 'User would like to view an individual project', %{
+  As a user I would like to
+  View the details of an individual project
+  So that I can see the progression of work
+} do
+
+  scenario 'A user tries to view the details of an individual project' do
+    project = FactoryGirl.create(:project)
+    visit project_path(project.id)
+
+    expect(page).to have_content(project.project_name)
+    expect(page).to have_content(project.project_type)
+    expect(page).to have_content(project.stage)
+    expect(page).to have_content(project.status)
+  end
+
+end
