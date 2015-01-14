6651f01eac4ff9b816a1a2294fe192a68922ef09
kennpat <kennedypr@gmail.com>
2015-01-14 16:43:11 -0500

    adding line to try and fix the travis build seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,10 @@
 #   cities = City.create([{ cycle_name: 'Chicago' }, { cycle_name: 'Copenhagen' }])
 #   Mayor.create(cycle_name: 'Emanuel', city: cities.first)
 
-# Cycle.create([{cycle_name: 'Cash'}, {cycle_name: 'Accounts Receivable'}, {cycle_name: 'Prepaid Assets'},
-#   {cycle_name: 'Inventory'}, {cycle_name: 'Capital Assets'}, {cycle_name: 'Intangible Assets'},
-#   {cycle_name: 'Investments'}, {cycle_name: 'Other Assets'},{cycle_name: 'Accounts Payable'},
-#   {cycle_name: 'Accrued Expenses'}, {cycle_name: 'Debt'}, {cycle_name: 'Equity'}, {cycle_name: 'Revenue'},
-#   {cycle_name: 'Operating Expenses'}, {cycle_name: 'Income Taxes'}, {cycle_name: 'Reporting'},
-#   {cycle_name: 'Concluding'}])
+Cycle.delete_all
+Cycle.create([{cycle_name: 'Cash'}, {cycle_name: 'Accounts Receivable'}, {cycle_name: 'Prepaid Assets'},
+  {cycle_name: 'Inventory'}, {cycle_name: 'Capital Assets'}, {cycle_name: 'Intangible Assets'},
+  {cycle_name: 'Investments'}, {cycle_name: 'Other Assets'},{cycle_name: 'Accounts Payable'},
+  {cycle_name: 'Accrued Expenses'}, {cycle_name: 'Debt'}, {cycle_name: 'Equity'}, {cycle_name: 'Revenue'},
+  {cycle_name: 'Operating Expenses'}, {cycle_name: 'Income Taxes'}, {cycle_name: 'Reporting'},
+  {cycle_name: 'Concluding'}])
--- spec/features/user_wants_to_remove_a_cycle_from_a_project_spec.rb
+++ spec/features/user_wants_to_remove_a_cycle_from_a_project_spec.rb
@@ -1,1 +1,17 @@
 require 'rails_helper'
+
+feature 'User wants to remove a cycle from the Project', %{
+  As a user
+  I would like to remove a cycle
+  So that I have only the ones necessary to that project
+} do
+
+  before :each do
+    @project = FactoryGirl.create(:project)
+  end
+
+  scenario 'User tries to remove a cycle from an existing project' do
+
+  end
+
+end
