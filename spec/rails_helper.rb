19b0c238a8649df846d726bfa49b90bc730b8cd3
kennpat <kennedypr@gmail.com>
2015-05-26 20:29:04 -0400

    Adding in seeded data for Travis testing

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,6 +38,9 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+  config.before(:suite) do
+    require "#{Rails.root}/db/seeds.rb"
+  end
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
